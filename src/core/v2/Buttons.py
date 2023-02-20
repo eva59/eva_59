eeeb0b1bc77dd8f4a37a7f113b08b3eaea23e5b6
eva59 <svyatoslav23112005@gmail.com>
2023-02-21 00:28:56 +0300

    v4

--- src/core/v2/Buttons.py
+++ src/core/v2/Buttons.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from telebot.types import KeyboardButton
 
 # from pydantic import BaseModel  # noqa
@@ -9,9 +10,9 @@ class Buttons(Enum):
     daily_schedule = KeyboardButton("Узнать расписание на день", callback_data= 'day')
     weekly_schedule = KeyboardButton("Узнать расписание на неделю", callback_data= 'week')
     back = KeyboardButton("Назад", callback_data = 'back')
-    monday = KeyboardButton("Понедельник")
-    tuesday = KeyboardButton("Вторник")
-    wednesday = KeyboardButton("Среда")
-    thursday = KeyboardButton("Четверг")
-    friday = KeyboardButton("Пятница")
-    saturday = KeyboardButton("Суббота")
+    monday = KeyboardButton("Понедельник", callback_data ='monday')
+    tuesday = KeyboardButton("Вторник", callback_data ='tuesday')
+    wednesday = KeyboardButton("Среда", callback_data =' wednesday')
+    thursday = KeyboardButton("Четверг", callback_data ='thursday')
+    friday = KeyboardButton("Пятница", callback_data ='friday')
+    saturday = KeyboardButton("Суббота", callback_data ='saturday')
--- src/core/v2/raspbot_2_0.py
+++ src/core/v2/raspbot_2_0.py
@@ -1,8 +1,10 @@
 import telebot as tb
 from telebot import types
 from ..settings import settings
-from utils import Buttons 
-#ScheduleFolk, WeekDays, ScheduleString, 
+from Buttons import Buttons
+from consts import choose, back
+
+from schedule import ScheduleFolk,  ScheduleString , WeekDays
 
 bot = tb.TeleBot(settings.telegram_token.get_secret_value())
 
@@ -28,26 +30,80 @@ def answer(call,message):
     if call.data == 'folk':
         markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True)
         markup1.add(Buttons.daily_schedule, Buttons.weekly_schedule)
-        bot.send_message(message.chat.id,"Выберите,что вам нужно,если хотите вернуться в меню просто нажмите кнопку 'Назад в меню' ",reply_markup=markup1)
-    
+        bot.send_message(message.chat.id,choose,reply_markup=markup1)
+
+        if call.data == 'day':
+            markup2 = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            markup2.add(Buttons.monday, Buttons.tuesday, Buttons.wednesday, Buttons.thursday, Buttons.friday,Buttons.saturday, Buttons.back)
+            bot.send_message(message.chat.id,choose,reply_markup=markup2)
+
+            if call.data == 'monday':
+                bot.send_message(message.chat.id,ScheduleFolk.monday,reply_markup=2)
+
+            elif call.data == 'tuesday':
+                bot.send_message(message.chat.id,ScheduleFolk.tuesday,reply_markup=2)
+
+            elif call.data == 'wednesday':
+                bot.send_message(message.chat.id,ScheduleFolk.wednesday,reply_markup=2)
+
+            elif call.data == 'thursday':
+                bot.send_message(message.chat.id,ScheduleFolk.thursday,reply_markup=2)
+
+            elif call.data == 'friday':
+                bot.send_message(message.chat.id,ScheduleFolk.friday,reply_markup=2)
+            
+            elif call.data == 'saturday':
+                bot.send_message(message.chat.id,ScheduleFolk.saturday,reply_markup=2)
+            
+            elif call.data == 'back':
+                bot.send_message(message.chat.id, back,reply_markup=1)
+
+        elif call.data =='week':
+            bot.send_message(message.chat.id,WeekDays.monday,ScheduleFolk.monday,WeekDays.tuesday,ScheduleFolk.tuesday,WeekDays.wednesday,ScheduleFolk.wednesday,WeekDays.thursday,ScheduleFolk.thursday,WeekDays.friday,ScheduleFolk.friday,WeekDays.saturday,ScheduleFolk.saturday,reply_markup=markup1)
+            markup4 = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            markup4.add(Buttons.back)
+            bot.send_message(message.chat.id, back,reply_markup=1)
+             
     elif call.data == 'string':
-        markup2 = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        markup2.add(Buttons.daily_schedule, Buttons.weekly_schedule)
-        bot.send_message(message.chat.id,"Выберите,что вам нужно,если хотите вернуться в меню просто нажмите кнопку 'Назад в меню' ",reply_markup=markup1)
+        markup4 = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup4.add(Buttons.daily_schedule, Buttons.weekly_schedule)
+        bot.send_message(message.chat.id,choose,reply_markup=markup1)
+       
+
+        if call.data == 'day':
+            markup5 = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            markup5.add(Buttons.monday, Buttons.tuesday, Buttons.wednesday, Buttons.thursday, Buttons.friday,Buttons.saturday, Buttons.back)
+            bot.send_message(message.chat.id,choose,reply_markup=5)
+
+            if call.data == 'monday':
+                bot.send_message(message.chat.id,ScheduleString.monday,reply_markup=5)
+
+            elif call.data == 'tuesday':
+                bot.send_message(message.chat.id,ScheduleString.tuesday,reply_markup=5)
+
+            elif call.data == 'wednesday':
+                bot.send_message(message.chat.id,ScheduleString.wednesday,reply_markup=5)
+
+            elif call.data == 'thursday':
+                bot.send_message(message.chat.id,ScheduleString.thursday,reply_markup=5)
+
+            elif call.data == 'friday':
+                bot.send_message(message.chat.id,ScheduleString.friday,reply_markup=5)
+            
+            elif call.data == 'saturday':
+                bot.send_message(message.chat.id,ScheduleString.saturday,reply_markup=5)
+            
+            elif call.data == 'back':
+                bot.send_message(message.chat.id, back,reply_markup=4)
+
+        elif call.data =='week':
+            bot.send_message(message.chat.id,WeekDays.monday,ScheduleString.monday,WeekDays.tuesday,ScheduleString.tuesday,WeekDays.wednesday,ScheduleString.wednesday,WeekDays.thursday,ScheduleString.thursday,WeekDays.friday,ScheduleString.friday,WeekDays.saturday,ScheduleString.saturday,reply_markup=markup4)
+            markup4 = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            markup4.add(Buttons.back)
+            bot.send_message(message.chat.id, back,reply_markup=4)
     
     elif call.data == 'back':
-        bot.send_message(message.chat.id, "Вы вернулись в меню",reply_markup=None)
-
-    
-
-
-
-
-
-
-
-
-
+        bot.send_message(message.chat.id, back,reply_markup=None)
 
 @bot.message_handler(
     func=lambda message: True,
